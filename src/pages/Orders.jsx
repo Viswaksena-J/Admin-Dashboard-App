a0c32edefd0762dc2ff5ccc766c9b1a361ae9fe0
Viswaksena Jayam <97895225+Viswaksena-J@users.noreply.github.com>
2024-02-13 18:20:22 +0530

    Orders done

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Orders = () => {
   return (
-    <div>Orders</div>
-  )
-}
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        // pageSettings={{ pageSize: 10 }}
+        allowSorting={true}
+        // allowFiltering={true}
+        // allowExcelExport={true}
+        // allowPdfExport={true}
+        // allowResizing={true}
+        // allowTextWrap={true}
+        // allowReordering={true}
+        // contextMenuItems={contextMenuItems}
+        // allowEditing={true}
+        // editSettings={{
+        //   allowEditing: true,
+        //   allowAdding: true,
+        //   allowDeleting: true,
+        // }}
+        // toolbar={[
+        //   "Add",
+        //   "Edit",
+        //   "Delete",
+        //   "Update",
+        //   "Cancel",
+        //   "Search",
+        //   "ExcelExport",
+        //   "PdfExport",
+        // ]}
+        // height="400"
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            PdfExport,
+            ExcelExport,
+            Edit,
+            Page,
+          ]}
+        />
+      </GridComponent>
+    </div>
+  );
+};
 
-export default Orders
+export default Orders;
